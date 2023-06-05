68582d1682843a184e5334c5439a5865149f59b5
gavigowda <115070861+gavigowda@users.noreply.github.com>
2023-06-05 13:26:11 +0530

    Update main.tf

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  # This module is now only being tested with Terraform 1.0.x. However, to make upgrading easier, we are setting
+  ## This module is now only being tested with Terraform 1.0.x. However, to make upgrading easier, we are setting
   # 0.13.0 as the minimum version, as that version has support for module depends_on, which we need for the ECS cluster.
   #  required_version = ">= 0.12.26"
   required_version = ">= 1.0.0"
